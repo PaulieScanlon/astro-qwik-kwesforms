21ed5b4f4d1c8a74457ecd85d03c03b69485339e
Paul Scanlon <pauliescanlon@gmail.com>
2024-02-15 17:29:55 -0800

    test: add document-ready strategy

--- src/componets/index-form.jsx
+++ src/componets/index-form.jsx
@@ -2,10 +2,13 @@ import { component$, useVisibleTask$ } from '@builder.io/qwik';
 import kwesforms from 'kwesforms';
 
 const IndexForme = component$(() => {
-  useVisibleTask$(() => {
-    console.log('useVisibleTask');
-    kwesforms.init();
-  });
+  useVisibleTask$(
+    () => {
+      console.log('useVisibleTask');
+      kwesforms.init();
+    },
+    { strategy: 'document-ready' }
+  );
 
   return (
     <form class='kwes-form' no-reload action='https://kwesforms.com/api/foreign/forms/1sp8Iy1rGETSLFGKqYVR'>
