f411c1c51436988cf42f09a374fa2d83b3c7db0a
Paul Scanlon <pauliescanlon@gmail.com>
2024-02-15 16:41:37 -0800

    test: add onclick example

---
+++ src/componets/onclick-form.jsx
@@ -0,0 +1,30 @@
+import { component$, useSignal, $ } from '@builder.io/qwik';
+
+const OnsubmitForm = component$(() => {
+  const hasLoaded = useSignal(false);
+
+  const handleClick = $(async () => {
+    console.log('handleClick');
+    const kwesforms = await import('kwesforms');
+    await kwesforms.init();
+    hasLoaded.value = true;
+  });
+
+  return (
+    <form
+      class='kwes-form'
+      no-reload
+      action='https://kwesforms.com/api/foreign/forms/1sp8Iy1rGETSLFGKqYVR'
+      onClick$={handleClick}
+    >
+      <label for='name'>Your Name</label>
+      <input type='text' name='name' rules='required|max:5' />
+
+      <button type='submit' disabled={!hasLoaded.value}>
+        Submit
+      </button>
+    </form>
+  );
+});
+
+export default OnsubmitForm;
